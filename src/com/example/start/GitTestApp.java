11839493012938acea51bc65aa7e5a8b47559e53
Avaz Babayev <avazbabayev@gmail.com>
2016-12-21 16:45:25 +0400

    new class

---
+++ src/com/example/start/GitTestApp.java
@@ -0,0 +1,10 @@
+package com.example.start;
+
+public class GitTestApp {
+
+	public static void main(final String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git, from branch bug#1234 !!");
+	}
+
+}
